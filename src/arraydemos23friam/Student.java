60b8ebf0e7e93b4b68a97430f70ba034c1fa5b03
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-19 09:36:29 -0400

    added phoneNo attribute

--- src/arraydemos23friam/Student.java
+++ src/arraydemos23friam/Student.java
@@ -6,6 +6,7 @@ public class Student {
     //comment added at 9:27AM
     private String sId;
     private String sName;
+    private String phoneNo;
 
     public Student(String sId, String sName) {
         this.sId = sId;
