7e6741664eb12504927407d4d25a76ec1864f3f3
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2023-05-26 13:24:24 -0400

    added new class

---
+++ src/arraydemos23friam/PartTimeStudent.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemos23friam;
+
+/**
+ *
+ * @author Ronak
+ */
+public class PartTimeStudent {
+    
+}
